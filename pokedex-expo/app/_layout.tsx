aa57c9fc6eb5158818031c986394b444643e66b8
aryaperdana-guud <Arya.Perdana@guud.company>
2024-02-21 17:30:54 +0700

    pokelist

--- pokedex-expo/app/_layout.tsx
+++ pokedex-expo/app/_layout.tsx
@@ -1,11 +1,14 @@
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
+import { Link, Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 
 import { useColorScheme } from '@/components/useColorScheme';
+import { GluestackUIProvider } from '@gluestack-ui/themed';
+import { config } from "@gluestack-ui/config";
+import { Pressable } from 'react-native';
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -48,11 +51,31 @@ function RootLayoutNav() {
   const colorScheme = useColorScheme();
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+    <GluestackUIProvider config={config}>
       <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen 
+          name="index" 
+          options={{
+            title: 'Pokedex', 
+            headerTitleAlign:'center',
+            headerRight: () => (
+              <Link href="/modal" asChild>
+                <Pressable>
+                  {({ pressed }) => (
+                    <FontAwesome
+                      name="heart-o"
+                      size={25}
+                      color={'black'}
+                      style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
+                    />
+                  )}
+                </Pressable>
+              </Link>
+            ),
+            }}/>
+        {/* <Stack.Screen name="(tabs)" options={{ headerShown: false }} /> */}
         <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
       </Stack>
-    </ThemeProvider>
+    </GluestackUIProvider>
   );
 }
---
+++ pokedex-expo/app/index.tsx
@@ -0,0 +1,91 @@
+import React from 'react'
+import {Box, Input, InputField, InputSlot, InputIcon, Text, SearchIcon, Image} from '@gluestack-ui/themed'
+import { FlatList } from 'react-native';
+import { ListRenderItem } from 'react-native';
+
+
+interface listItf{
+    name: string;
+    url: string;
+}
+const DATA = [
+    {
+      "name": "charmander",
+      "url": "https://pokeapi.co/api/v2/pokemon/4/"
+    },
+    {
+      "name": "charmeleon",
+      "url": "https://pokeapi.co/api/v2/pokemon/5/"
+    },
+    {
+      "name": "charizard",
+      "url": "https://pokeapi.co/api/v2/pokemon/6/"
+    },
+    {
+      "name": "squirtle",
+      "url": "https://pokeapi.co/api/v2/pokemon/7/"
+    }
+  ];
+
+const renderItem: ListRenderItem<listItf> = ({ item }) => {
+    let pokeId=0;
+    const lastNumber = item.url.split('/').filter((part: any) => !!part).pop();
+    if (lastNumber && !isNaN(parseInt(lastNumber))) {
+        pokeId = parseInt(lastNumber);
+        console.log(pokeId);
+    }
+
+    return <Box
+    style={{
+        width: '50%', // Each item takes up 50% of the width
+        padding: 4, // Padding on the left, right, and between columns
+        
+    }}
+>
+    <Box
+        style={{
+            // aspectRatio: 1, // 1:1 aspect ratio
+            borderWidth: 1,
+            borderColor: 'black',
+            justifyContent: 'flex-start',
+            alignItems: 'center',
+            backgroundColor: 'white'
+        }}
+    >
+        <Box aspectRatio={1} w={'100%'}>
+            <Image 
+                size='full'
+                source={{uri: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokeId}.svg`}}
+                alt={item.name}
+            />
+        </Box>
+        <Text>{item.name}</Text>
+    </Box>
+    
+</Box>
+}
+
+export default function index() {
+  return (
+    <Box flex={1} padding={4}>
+      
+      <Box padding={4}>
+      <Input backgroundColor='white' borderColor='black'>
+          <InputSlot pl='$3'>
+            <InputIcon as={SearchIcon}/>
+          </InputSlot>
+          <InputField
+            placeholder="Search..."
+          />
+      </Input>
+      </Box>
+
+      <FlatList
+        data={DATA}
+        renderItem={renderItem}
+        keyExtractor={(item:any) => item.url}
+        numColumns={2}
+      />
+    </Box>
+  )
+}
